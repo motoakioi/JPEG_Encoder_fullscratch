d7e11ebc0816e74d393f22b15d33f6804edd4603
motoakioi <oi@comp.sd.keio.ac.jp>
2018-09-12 21:07:26 +0900

    [add] quantize calculation

--- src/quantize.cpp
+++ src/quantize.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 
 #include"quantize.h"
-//#include"quantize_table.h"
+#include"quantize_table.h"
 
 void quantize( double in[8][8], double out[8][8], double table[8][8] ){
+	for ( int y = 0; y < 8; y++ ){
+		for (int x = 0; x < 8; x++ ){
+			out[x][y] = in[x][y] / table[x][y];
+		}// for loop of x
+	}// for loop of y
 
 }
 
